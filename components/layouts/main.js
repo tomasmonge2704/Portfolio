25062a39a0465150b394b6707afef072daacfc44
Tomas Monge <tomasmonge@Tomass-MBP.fibertel.com.ar>
2023-06-05 17:49:55 -0300

    fix

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,20 +15,18 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
-        <meta name="author" content="Takuya Matsuyama" />
-        <meta name="author" content="craftzdog" />
+        <meta name="description" content="Tomas Monge homepage" />
+        <meta name="author" content="Tomas Monge" />
+        <meta name="author" content="Portfolio" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Takuya Matsuyama" />
+        <meta name="twitter:title" content="Tomas Monge" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama" />
-        <meta name="og:title" content="Takuya Matsuyama" />
+        <meta name="twitter:image" content="/card.png" />
+        <meta property="og:site_name" content="Tomas Monge" />
+        <meta name="og:title" content="Tomas Monge" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
+        <meta property="og:image" content="/card.png" />
         <title>Tomas Monge - Homepage</title>
       </Head>
 
